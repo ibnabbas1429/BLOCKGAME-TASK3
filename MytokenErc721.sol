d042c8105e37973f11f609772385bc2b72f7668a
Aminu Ismail <63140904+ibnabbas1429@users.noreply.github.com>
2022-04-15 17:32:48 +0000

    updating the ERC271 token

--- MytokenErc721.sol
+++ MytokenErc721.sol
@@ -1,9 +1,47 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract emaculatenonTOKEN is ERC721, ERC721URIStorage, Ownable {
+    constructor() ERC721("emaculatenonTOKEN", "ETT") {}
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyOwner
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+// A siimple function to calculate the reward
+ 
+
+}.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 
 contract TaskThreeTOKEN is ERC721, ERC721URIStorage, Ownable {
     constructor() ERC721("TaskThreeTOKEN", "ETT") {}
